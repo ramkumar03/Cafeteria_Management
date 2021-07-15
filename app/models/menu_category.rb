0d7049421e261348c242c06dcef4c9852f7942ad
ramkumar03 <roster99brock@gmail.com>
2021-07-15 16:35:35 +0530

    Added ActiveRecord Migration to menu_categories and menu_items table

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,9 +1,6 @@
 class MenuCategory < ActiveRecord::Base
-<<<<<<< HEAD
   has_many :menu_items
 
-=======
->>>>>>> 1c33e934be8f5368a240a0038933637eb5146548
   def to_category
     "#{id}. #{name} | Available: #{status}"
   end
