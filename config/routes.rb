27fb93d9f59084ed87bdec6e139ef00ab1fb7722
ramkumar03 <roster99brock@gmail.com>
2021-07-15 20:53:45 +0530

    Added Signout functionality and Homepage using Sessions MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/", to: "home#index"
+
   get "menu_categories", to: "menu_categories#index"
 
   resources :users
 
   get "/signin", to: "sessions#new", as: :new_sessions
   post "/signin", to: "sessions#create", as: :sessions
+  delete "/signout", to: "sessions#destroy", as: :destroy_session
 end
