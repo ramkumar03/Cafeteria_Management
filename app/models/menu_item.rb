b767e0f2a63a3926bf73e39b47457384fba14047
ramkumar03 <roster99brock@gmail.com>
2021-07-16 17:19:53 +0530

    Stylized Menu with description

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,11 +1,11 @@
 class MenuItem < ActiveRecord::Base
   belongs_to :menu_category
 
-  def self.get_items(id)
+  def self.get_menu_items(id)
     where(menu_category_id: id)
   end
 
-  def to_menu
-    "#{name} | Price: #{price} "
-  end
+  ##def to_menu
+  ##  "#{name} | Price: #{price} "
+  ##end
 end
