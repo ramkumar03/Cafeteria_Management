a850fe20528c677232d7acead3ebd5aa3c682a35
ramkumar03 <roster99brock@gmail.com>
2021-07-19 11:33:29 +0530

    Added Reports Page

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,16 @@
+class ReportsController < ApplicationController
+  def index
+    if @current_user.role == "owner"
+      if params[:date_first].present?
+        @orders = Order.where("ordered_at between ? and ?",
+                              params[:date_first],
+                              DateTime.parse(params[:date_last]) + 1)
+      else
+        @orders = Order.all
+      end
+      render "index"
+    else
+      render "/"
+    end
+  end
+end
