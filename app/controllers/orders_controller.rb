c9c64da46d23e35cf5bbea5f9f2de98b7e648738
ramkumar03 <roster99brock@gmail.com>
2021-07-23 18:15:30 +0530

    Fixed Walkin orders issue

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,26 +5,37 @@ class OrdersController < ApplicationController
   end
 
   def create
-    if @current_user.role != "customer"
+    if @current_user.role == "clerk"
       ordertype = false #walkin
-    else
-      ordertype = true #online
-    end
+      new_order = Order.new(
+        user_id: @current_user.id,
+        ordered_at: DateTime.now,
+        delivered_at: DateTime.now,
+        total_amount: params[:total_amount],
+        online_order: ordertype,
+      )
 
-    new_order = Order.new(
-      user_id: @current_user.id,
-      ordered_at: DateTime.now,
-      total_amount: params[:total_amount],
-      deliver_address: params[:deliver_address],
-      online_order: ordertype,
-    )
-
-    if new_order.save
-      flash[:success] = "Order Placed"
+      new_order.save
+      flash[:success] = "walk-in Order Placed"
       redirect_to create_order_item_path(id: new_order.id)
     else
-      flash[:error] = new_order.errors.full_messages.join(", ")
-      redirect_to "/"
+      ordertype = true #online
+      unless params[:deliver_address] == ""
+        new_order = Order.new(
+          user_id: @current_user.id,
+          ordered_at: DateTime.now,
+          total_amount: params[:total_amount],
+          deliver_address: params[:deliver_address],
+          online_order: ordertype,
+        )
+
+        new_order.save
+        flash[:success] = "Order Placed"
+        redirect_to create_order_item_path(id: new_order.id)
+      else
+        flash[:error] = "Delivery address can't be empty"
+        redirect_to "/"
+      end
     end
   end
 
